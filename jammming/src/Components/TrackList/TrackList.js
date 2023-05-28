3359f5c2d767d72e41b75b561ae248d593c9bcee
Cheryl Mauldin <cherylmauldin@Cheryls-Air.attlocal.net>
2023-05-28 14:15:49 -0500

    first commit

---
+++ jammming/src/Components/TrackList/TrackList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Track from '../Track/Track';
+import './TrackList.css';
+
+const TrackList = (props) => {
+  return (
+    <div className="TrackList">
+      {props.tracks.map((track) => {
+        return (
+          <Track
+            track={track}
+            key={track.id}
+            onAdd={props.onAdd}
+            isRemoval={props.isRemoval}
+            onRemove={props.onRemove}
+          />  
+        );
+      })}  
+    </div>
+  );
+};
+
+
+
+
+export default TrackList;
